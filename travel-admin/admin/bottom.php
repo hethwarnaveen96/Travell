f3a3c13bd58bfae9b343e5071a02c9daa1f3d716
Sudharsan7303 <sudharsan7303@gmail.com>
2024-09-29 20:25:41 +0530

    Project Uploaded

---
+++ travel-admin/admin/bottom.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<footer >
+            <div class="container py-4">
+            <div class="row">
+        
+            
+             <div class="col" id="f1">
+              <h3>Content</h3>
+              <p id="footerpara">Content to be added here...</p>
+             </div>
+             <div class="col links">
+              <h3>Quick links</h3>
+              <p>
+              <a href="./index.php">Home</a>
+              </p>
+              <p>
+              <a href="http://localhost/Travel/Travel/travellers-main/index.php">Web-Preview</a>
+              </p>
+             </div>
+             <div class="col" id="f2">
+               <h3>Contact</h3>
+              <p><i class="fa fa-home"></i> <a href="https://www.google.com/maps?q=kumbakonam+map&rlz=1C1CHBD_enIN1018IN1019&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiWvPeFxfT8AhV-SGwGHf94CiYQ_AUoAXoECAMQAw">Kumbakonam</a></p>
+              <p>Mail <i class="fa fa-envelope"></i> travellerschoice@gmail.com</p>
+              <p>Call <i class="fa fa-phone"></i> +91 8555238593</p>
+              <p>Follow Us <i class="fa fa-instagram" aria-hidden="true"></i> <i class="fa fa-twitter" aria-hidden="true"></i>
+        <i class="fa fa-whatsapp" aria-hidden="true"></i>
+        <i class="fa fa-facebook" aria-hidden="true"></i> </p>
+             </div>
+             
+            </div>
+            
+            </div>
+            <div class="container-fluid"id="f3">
+              <p id="footerend">copyright &copy;2023 Traveller's Choice by SASTRA</p>
+            </div>
+            
+</footer>
+</body>
+</html>
+
+<!-- <div class="copyright text-right">
+<p style="padding-right:100px"></p>
+</div> -->
+  
